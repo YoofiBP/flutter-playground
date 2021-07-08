0784d0b7f7ee7a7db3d5bf93243b4103708dc3fa
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2021-07-08 16:53:29 +0200

    sdjj

--- lib/views/screens/signup_screen.dart
+++ lib/views/screens/signup_screen.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import '../components/download_button.dart';
-=======
->>>>>>> parent of 84d4d80... Added download button
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
